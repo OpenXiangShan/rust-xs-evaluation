73c8d0ea0428883702b9fb5b5047f09f786ae738
SKTT1Ryze <linuxgnulover@gmail.com>
2020-12-28 21:35:18 +0800

    fix allocator error

--- tests/add/src/main.rs
+++ tests/add/src/main.rs
@@ -8,10 +8,10 @@
 use core::alloc::Layout;
 #[cfg(not(test))]
 use core::panic::PanicInfo;
-use linked_list_allocator::LockedHeap;
+// use linked_list_allocator::LockedHeap;
+use buddy_system_allocator::LockedHeap;
 use riscv::register::{
     mhartid,
-
 };
 
 global_asm!(include_str!("entry.asm"));
@@ -30,50 +30,24 @@ fn panic(_info: &PanicInfo) -> ! {
 fn oom(_layout: Layout) -> ! {
     loop {}
 }
+
 #[no_mangle]
 pub extern "C" fn rust_main() -> ! {
-    // unsafe {
-    //     llvm_asm!(
-    //         "
-    //     csrr    a2, mhartid
-    //     lui     t0, %hi(_max_hart_id)
-    //     add     t0, t0, %lo(_max_hart_id)
-    //     bgtu    a2, t0, _start_abort
-    //     la      sp, _stack_start
-    //     lui     t0, %hi(_hart_stack_size)
-    //     add     t0, t0, %lo(_hart_stack_size)
-    // .ifdef __riscv_mul
-    //     mul     t0, a2, t0
-    // .else
-    //     beqz    a2, 2f  // Jump if single-hart
-    //     mv      t1, a2
-    //     mv      t2, t0
-    // 1:
-    //     add     t0, t0, t2
-    //     addi    t1, t1, -1
-    //     bnez    t1, 1b
-    // 2:
-    // .endif
-    //     sub     sp, sp, t0
-    //     csrw    mscratch, zero
-    //     j _start_success
-        
-    // _start_abort:
-    //     wfi
-    //     j _start_abort
-    // _start_success:
-    // "
-    //     )
-    // };
-
     if mhartid::read() == 0 {
         extern "C" {
+            static mut _ebss: u32;
+            static mut _sbss: u32;
+            static mut _edata: u32;
+            static mut _sdata: u32;
+            static _sidata: u32;
             fn _sheap();
             fn _head_size();
         }
         let sheap = &mut _sheap as *mut _ as usize;
         let heap_size = &_head_size as *const _ as usize;
         unsafe {
+            r0::zero_bss(&mut _sbss, &mut _ebss);
+            r0::init_data(&mut _sdata, &mut _edata, &_sidata);
             ALLOCATOR.lock().init(sheap, heap_size);
         }
     }
