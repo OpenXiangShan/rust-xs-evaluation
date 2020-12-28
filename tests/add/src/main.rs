8aba78d08c77854ac4cbd2b8833ce050e3291a68
hustccc <1276675421@qq.com>
2020-12-29 05:27:33 +0900

    fix something

--- tests/add/src/main.rs
+++ tests/add/src/main.rs
@@ -14,59 +14,57 @@ use riscv::register::{
 
 };
 
+global_asm!(include_str!("entry.asm"));
 
 #[global_allocator]
 static ALLOCATOR: LockedHeap = LockedHeap::empty();
 
 #[cfg(not(test))]
 #[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
 #[cfg(not(test))]
 #[alloc_error_handler]
-fn oom(layout: Layout) -> ! {
+fn oom(_layout: Layout) -> ! {
     loop {}
 }
-
-#[export_name = "_start"]
-#[link_section = ".text.entry"] // this is stable
-fn main() -> ! {
-    unsafe {
-        llvm_asm!(
-            "
-        csrr    a2, mhartid
-        lui     t0, %hi(_max_hart_id)
-        add     t0, t0, %lo(_max_hart_id)
-        bgtu    a2, t0, _start_abort
-        la      sp, _stack_start
-        lui     t0, %hi(_hart_stack_size)
-        add     t0, t0, %lo(_hart_stack_size)
-    .ifdef __riscv_mul
-        mul     t0, a2, t0
-    .else
-        beqz    a2, 2f  // Jump if single-hart
-        mv      t1, a2
-        mv      t2, t0
-    1:
-        add     t0, t0, t2
-        addi    t1, t1, -1
-        bnez    t1, 1b
-    2:
-    .endif
-        sub     sp, sp, t0
-        csrw    mscratch, zero
-        j _start_success
+#[no_mangle]
+pub extern "C" fn rust_main() -> ! {
+    // unsafe {
+    //     llvm_asm!(
+    //         "
+    //     csrr    a2, mhartid
+    //     lui     t0, %hi(_max_hart_id)
+    //     add     t0, t0, %lo(_max_hart_id)
+    //     bgtu    a2, t0, _start_abort
+    //     la      sp, _stack_start
+    //     lui     t0, %hi(_hart_stack_size)
+    //     add     t0, t0, %lo(_hart_stack_size)
+    // .ifdef __riscv_mul
+    //     mul     t0, a2, t0
+    // .else
+    //     beqz    a2, 2f  // Jump if single-hart
+    //     mv      t1, a2
+    //     mv      t2, t0
+    // 1:
+    //     add     t0, t0, t2
+    //     addi    t1, t1, -1
+    //     bnez    t1, 1b
+    // 2:
+    // .endif
+    //     sub     sp, sp, t0
+    //     csrw    mscratch, zero
+    //     j _start_success
         
-    _start_abort:
-        wfi
-        j _start_abort
-    _start_success:
-        
-    "
-        )
-    };
+    // _start_abort:
+    //     wfi
+    //     j _start_abort
+    // _start_success:
+    // "
+    //     )
+    // };
 
     if mhartid::read() == 0 {
         extern "C" {
