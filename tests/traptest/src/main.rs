98ca9ca9ee896703bb2ac5471e86f04e122863c0
hustccc <1276675421@qq.com>
2021-01-10 02:10:30 +0900

    add trap test

---
+++ tests/traptest/src/main.rs
@@ -0,0 +1,70 @@
+#![no_std]
+#![no_main]
+#![feature(alloc_error_handler)]
+#![feature(global_asm)]
+#![feature(llvm_asm)]
+
+extern crate alloc;
+extern crate xs_hal;
+extern crate ansi_rgb;
+
+#[macro_use]
+mod device;
+
+#[cfg(not(test))]
+use core::alloc::Layout;
+#[cfg(not(test))]
+use core::panic::PanicInfo;
+use buddy_system_allocator::LockedHeap;
+use ansi_rgb::{ Foreground, red};
+use riscv::register::{mhartid};
+use xs_hal::{XSPeripherals, hit_trap};
+use xs_rt::{entry, pre_init};
+
+#[global_allocator]
+static ALLOCATOR: LockedHeap = LockedHeap::empty();
+
+static mut XSPERIPHERALS: XSPeripherals = XSPeripherals::new(); 
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("[xs] {}", info);
+    hit_trap(1);
+    // should not reach here
+    loop {}
+}
+
+#[cfg(not(test))]
+#[alloc_error_handler]
+fn oom(_layout: Layout) -> ! {
+    // oom hit the bad trap
+    hit_trap(1);
+    loop {}
+}
+
+extern "C" {
+    static _sheap: u8;
+    static _heap_size: u8;
+}
+
+#[pre_init]
+unsafe fn before_main() {
+    // TODO
+}
+
+#[entry]
+#[no_mangle]
+fn main() -> ! {
+    unsafe {
+        let heap_bottom = &_sheap as *const u8 as usize;
+        let heap_size = &_heap_size as *const u8 as usize;
+        ALLOCATOR.lock().init(heap_bottom, heap_size);
+    }
+    device::init();
+    device::print_logo();
+    println!("[{}] XiangShan core {} is running", "xs".fg(red()), mhartid::read());
+    // unsafe { llvm_asm!("mv a0, $0; .word 0x0005006b" :: "r"(!is_pass) :: "volatile"); }
+    hit_trap(0);
+    loop {}
+}
